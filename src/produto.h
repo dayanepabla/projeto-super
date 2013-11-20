12e2a6c5e18182f278dc459ac158160eb89776e6
Dayane Pabla <dayanepabla@dayanepabla.(none)>
2013-11-19 23:15:03 -0300

    inicio

---
+++ src/produto.h
@@ -0,0 +1,12 @@
+struct Produto {
+	char nome [50];
+    char categoria [20];
+	int codigo;
+	float preco;
+	char fabricante [20];
+    char descricao [200];
+	int validade[3];
+	char localizacao;
+} registros[100];
+
+
