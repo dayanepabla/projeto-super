eacc098bec0285adb781a776e6b429d7a0d917a1
Dayane Pabla <dayanepabla@dayanepabla.(none)>
2013-11-22 00:11:57 -0300

    Adiciona o campo validade e implementa os prototipos das funções de p… …

--- src/produto.h
+++ src/produto.h
@@ -1,12 +1,30 @@
-struct Produto {
-	char nome [50];
-    char categoria [20];
-	int codigo;
-	float preco;
-	char fabricante [20];
-    char descricao [200];
-	int validade[3];
-	char localizacao;
-} registros[100];
+#include <stdio.h>
+#include "data.h"
 
+struct _produto {
+    char *nome;
+    char *categoria;
+    int codigo;
+    float preco;
+    int quantidade;
+    char *fabricante;
+    char *descricao;
+    Data validade;
+    int corredor;
+    int prateleira;
 
+};
+
+typedef struct _produto Produto;
+
+/*
+ * Salva um produto no banco de dados.
+ */
+int produto_salvar (Produto prod, FILE *bd);
+
+/*
+ * Retorna um objeto do tipo produto.
+ */
+Produto produto_novo(char *nome, char *categoria, int codigo, float preco, int quantidade,
+                     char *fabricante, char *descricao, Data validade, int corredor,
+                     int prateleira);
