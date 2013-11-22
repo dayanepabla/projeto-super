72dcdec883caf0ec72397b7a25ec95eae922a3c3
Dayane Pabla <dayanepabla@dayanepabla.(none)>
2013-11-22 00:10:13 -0300

    Cria tipo data para ser utilizado nos produtos

---
+++ src/data.c
@@ -0,0 +1,11 @@
+#include "data.h"
+
+ Data data_novo(int ano, int mes, int dia){
+    Data nova_data;
+
+    nova_data.ano = ano;
+    nova_data.mes = mes;
+    nova_data.dia = dia;
+
+    return nova_data;
+ }
---
+++ src/data.h
@@ -0,0 +1,12 @@
+struct _data{
+    int ano;
+    int mes;
+    int dia;
+};
+
+typedef struct _data Data;
+
+/*
+* Retorna um objeto do tipo data.
+*/
+Data data_novo(int ano, int mes, int dia);
