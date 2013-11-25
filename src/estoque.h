fbdac7504ddb3d02c3c33a6d1a9a7e6c8a4fb171
Dayane Pabla <dayanepabla@gmail.com>
2013-11-25 16:17:54 -0300

    Adição das funções novo estoque e busca por nome do produto

--- src/estoque.h
+++ src/estoque.h
@@ -1,7 +1,16 @@
 struct _estoque {
-    int qtd_produtos = 0;
+    int qtd_produtos;
     Produto *produtos;
 };
 
 typedef struct _estoque Estoque;
 
+/*
+ * Inicializa um produto.
+ */
+Estoque estoque_novo (int qtd_produtos, Produto *produtos);
+
+/*
+ * Busca um produto por nome e retorna um ponteiro para o produto.
+ */
+Produto* estoque_busca_nome (Estoque estoque, char *nome_produto);
