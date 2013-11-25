605c84978ca57e453e961b18bd9921d096a713e4
Diego Toral <diegotoral@gmail.com>
2013-11-25 10:35:53 -0300

    Criação do construtor de estoque

--- src/estoque.c
+++ src/estoque.c
@@ -1,1 +1,9 @@
 #include "estoque.h"
+
+ Estoque estoque_novo (int qtd_produtos, Produto *produtos) {
+    Estoque novo_estoque;
+    novo_estoque.qtd_produtos = qtd_produtos;
+    novo_estoque.produtos = produtos;
+
+    return novo_estoque;
+ }
--- src/estoque.h
+++ src/estoque.h
@@ -1,7 +1,13 @@
+#include "produto.h"
+
 struct _estoque {
-    int qtd_produtos = 0;
+    int qtd_produtos;
     Produto *produtos;
 };
 
 typedef struct _estoque Estoque;
 
+/*
+ * Inicializa um produto.
+ */
+ Estoque estoque_novo (int qtd_produtos, Produto *produtos);
