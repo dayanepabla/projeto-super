f86b749ea0ca809c680c6e646dc58f4036439733
Dayane Pabla <dayanepabla@gmail.com>
2013-11-27 21:56:26 -0300

    Teste das novas funcionalidades

--- src/super.c
+++ src/super.c
@@ -1,88 +1,65 @@
 #include <stdio.h>
+//#include "estoque.h"
 #include "produto.h"
-// #inclusw "data.h"
-
-//Função cadastrar
-//Função Busca
-//Abri, alterar e salvar arquivo
-//Ordenar datas de validades
-//Fução alerta
-	//Data de validade
-	//Estoque baixo
-//Função carrinho
 
 int main () {
 
     FILE *bd;
-    Produto prod;
-    Data mes;
+    Produto *prod;
+    Data *mes;
     bd = fopen ("bd.txt", "w");
     mes = data_novo (2013, 12, 1);
     prod = produto_novo ("Mouse", "Eletronico", 1300, 34.20, 500, "Microsoft", "Mouse sem fio",
                           mes, 2, 5);
-    printf("%s, %s, %i, %f, %i, %s, %s, %i/%i/%i, %i, %i\n", prod.nome, prod.categoria, prod.codigo, prod.preco,
-                                                             prod.quantidade, prod.fabricante, prod.descricao,
-                                                             prod.validade.ano, prod.validade.mes,
-                                                             prod.validade.dia, prod.corredor, prod.prateleira);                                                      ;
+    printf("%s, %s, %i, %f, %i, %s, %s, %i/%i/%i, %i, %i\n", prod->nome, prod->categoria, prod->codigo, prod->preco,
+                                                             prod->quantidade, prod->fabricante, prod->descricao,
+                                                             prod->validade->ano, prod->validade->mes,
+                                                             prod->validade->dia, prod->corredor, prod->prateleira);                                                      ;
     // printf("%i/%i/%i\n", mes.ano, mes.mes, mes.dia);
     produto_salvar(prod, bd);
     printf ("Rodando\n");
 
+ /*   Estoque *estoque;
+    char *nome, *categoria, *fabricante, *descricao;
+    int codigo, quantidade, corredor, prateleira;
+    float preco;
+    Data validade;
+
+    estoque = estoque_novo (0, NULL);
+    nome = categoria = fabricante = descricao = NULL;*/
+/*
+        printf("Quantidade de itens no estoque: %i\n", estoque->qtd_produtos);
+        printf("\t\t\tCADASTRAR PRODUTO\t\t\t\n");
+
+        printf("Nome: ");
+        scanf("%s", nome);
+
+        printf("categoria: ");
+        scanf("%s", categoria);
+
+        printf("Código: ");
+        scanf("%i", &codigo);
+
+        printf("Preço: ");
+        scanf("%f", &preco);
+
+        printf("Quantidade: ");
+        scanf("%i", &quantidade);
+
+        printf("Fabricante: ");
+        scanf("%s", fabricante);
+
+        printf("Descricao: ");
+        scanf("%s", descricao);
+
+        printf("Validade do produto: \n");
+        scanf("%i %i %i\n", &validade.ano, &validade.mes, &validade.dia);
+
+        printf("corredor: ");
+        scanf("%i", &corredor);
+
+        printf("Prateleira: ");
+        scanf("%i", &prateleira);*/
+
     return 0;
 }
-
-/*int main () {
-    int op, op_f, op_c, cad;
-
-    printf("\n\n\t\t\t\t SUPERMERCADO \n\n\n");
-    printf("MENU\n\n1 - Funcionário\n2 - Cliente\n3 - Sair\n\nOpção: ");
-    scanf("%d",&op);
-    switch ( op ) {
-        case 1:
-        {
-        	printf("MENU\n\n1 - Buscar produto\n2 - Repor estoque\n3 - Cadastrar novos produtos\n4 - Sair\n\nOpção: ");
-        	scanf ("%d", &op_f);
-
-                if (op_f == 1 ){
-                	//Função Busca
-                }
-                if (op_f == 2){
-                    //Produto
-                    //Add ao banco de dados
-                }
-                if (op_f == 3) {
-                	printf("Cadastrar novo produto por:\n1 - Teclado\n2 - Arquivo\n");
-                	scanf ("%d",&cad);
-                	if (cad == 1){
-                		//Função Produto
-                		//Add ao banco de dados
-                	} else {
-                		//Carregar arquivo
-                		//Add ao banco de dados
-                	}
-                }
-                if (op_f == 4) {
-                	break;
-                }
-        }
-        case 2:
-        {
-            printf("MENU\n\n1 - Buscar produto\n2 - Carrinho de compras\n3 - Sair\n\nOpção: ");
-            scanf ("%d", &op_c);
-            	if (op_c == 1) {
-            		//Função Busca
-            	}
-            	if (op_c == 2) {
-            		//Função carrinho
-            	} else {
-            		break;
-            	}
-        }
-        case 3:
-        {
-            break;
-        }
-
-    return 0;
-}*/
-
