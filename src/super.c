eaac5e90d348542fcd40b944112e80b6fcde8000
Dayane Pabla <dayanepabla@gmail.com>
2013-11-30 22:37:21 -0300

    Menu

--- src/super.c
+++ src/super.c
@@ -1,24 +1,103 @@
 #include <stdio.h>
+#include <assert.h>
 #include "carrinho.h"
+
+#define BD_NAME "bd.txt"
+
+Estoque *estoque;
+
+/*
+ * Inicialização do sistema.
+ */
+void init ();
+
+/*
+ * Menu inicial.
+ */
+void inicial ();
+
+/*
+ * Encerra o programa.
+ */
+void sair ();
+
+/*
+ * Exibe a tela de cliente.
+ */
+void cliente ();
+
+/*
+ * Exibe a tela funcionário.
+ */
+void funcionario ();
+
+/*
+ * Exibe a tela de busca.
+ */
+void busca ();
+
 
 int main () {
 
-    Data *data;
-    Produto *prod;
-    EStoque *estoque;
+    init();
+    inicial ();
+    sair ();
+    return 0;
+}
 
-    data = data_novo (2013, 12, 1);
-    estoque = estoque_novo (0, NULL);
-    prod = produto_novo ("Mouse", "Eletronico", 1300, 34.20, 500, "Microsoft", "Mouse sem fio",
-                         data, 2, 5);
+void init () {
+    FILE *bd;
+    bd = fopen(BD_NAME, "a");
 
-    estoque_add_produto (estoque, prod);
+    // Verifica se o arquivo abriu com sucesso.
+    assert(bd != NULL);
+}
 
-    // printf("%d\n", estoque_cheio (estoque));
-    estoque_repor (estoque, 1300, data_novo(2014, 01, 01), 10);
+void inicial () {
 
-    estoque_listar(estoque);
-    // produto_listar (estoque_busca_fabricante (estoque, "Micr osoft"));
+    int op;
+
+    printf("\t\t\tSUPERMERCADO\t\t\t\n");
+    printf("1. Cliente\n2. Funcionário\n3. Sair\n");
+    scanf ("%i", &op);
+    if (op == 1){
+        cliente();
+    } if (op == 2) {
+        funcionario();
+    }
+}
+
+void busca () {
+    int op;
+
+    printf("Buscar produto\n");
+    printf("1. Buscar por nome\n");
+    printf("2. Buscar por fabricante\n");
+    printf("3. Buscar por categoria\n");
+    printf("4. Sair\n");
+    scanf ("%i", &op);
+}
+
+void cliente (){
+    busca ();
+}
+
+void funcionario () {
+    int op;
+
+    printf("1. Buscar produto\n2. Repor estoque\n3. Cadastrar produtos novos\n");
+    scanf ("%i", &op);
+    switch (op) {
+        case 1:
+            busca();
+            break;
+        case 2:
+        case 3:
+        default:
+            printf("Opção inválida!\n");
+    }
+}
+
 
 
     return 0;
